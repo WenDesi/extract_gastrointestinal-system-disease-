37019602241c76ce2c6262e0f99d675258195090
wds2006sdo <641614152@qq.com>
2016-10-13 15:21:02 +0800

    entity align

---
+++ 实体对齐/build_same_doid.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+import csv
+import codecs
+import chardet
+
+def read_csv(filepath):
+    results = []
+    reader = csv.reader(file(filepath, 'rU'))
+    for line in reader:
+        results.append(line)
+    return results
+
+def write_csv(filepath,lines):
+    writer = csv.writer(file(filepath, 'wb'))
+    for line in lines:
+        writer.writerow(line)
+
+
+
+
+if __name__ == '__main__':
+    # 读入英文doid name 对应列表
+    eng_filepath = 'doid_name_pairs.csv'
+    eng_list = read_csv(eng_filepath)
+    eng_dict = {}
+    for line in eng_list:
+        eng_dict[line[1]] = line[0]
+
+    # 读入中英对照表格，并构建中文词典
+    zh_filepath = '../ontology/zh_diseaseName.csv'
+    zh_list = read_csv(zh_filepath)
+    zh_dict = {}
+    for line in zh_list:
+        id = eng_dict[line[1]]
+        zh_ = line[:1] + line[2:]
+
+        for zh in zh_:
+            zh_dict[zh] = id
+
+    # 读入对齐实体列表并找到doid
+    same_filepath = 'same_entity.csv'
+    same_list = read_csv(same_filepath)
+    for i in xrange(len(same_list)):
+        id = zh_dict[same_list[i][2]]
+        same_list[i][0] = id
+
+
+    # 结果写入
+    write_csv('same_entity_with_doid.csv',same_list)
+
+
+
