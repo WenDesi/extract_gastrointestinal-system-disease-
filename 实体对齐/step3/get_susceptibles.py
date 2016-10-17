f158db9e3310e904493afd6761f0ab9336249057
wds2006sdo <641614152@qq.com>
2016-10-17 15:14:58 +0800

    step 4

---
+++ 实体对齐/step3/get_susceptibles.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import csv
+
+from SQLConnector import *
+
+def read_csv(filepath):
+    results = []
+    reader = csv.reader(file(filepath, 'rU'))
+    for line in reader:
+        results.append(line)
+    return results
+
+def write_csv(filepath,llist):
+    writer = csv.writer(file(filepath, 'wb'))
+    for line in llist:
+        writer.writerow(line)
+
+def get_department(sqlConn,id):
+    sql = 'select multiple_population from neike where id = %s;' % id
+    result = sqlConn.select(sql)
+
+    return result[0][0]
+
+
+if __name__ == '__main__':
+    doid_filepath = 'same_entity_with_doid.csv'
+    department_filepath = 'susceptibles.csv'
+
+    doid_lines = read_csv(doid_filepath)
+
+    result = []
+    sql_conn = SqlConnector(db='disease_ontology')
+    for line in doid_lines:
+        doid = line[0]
+        department = get_department(sql_conn,line[1])
+
+        result.append([doid,department])
+
+    write_csv(department_filepath,result)
--- 实体对齐/step4/body_part.py
+++ 实体对齐/step4/body_part.py
@@ -17,7 +17,7 @@ def read_ontology(filepath):
     file_obj = codecs.open(filepath,'r','utf-8')
     while True:
         line = file_obj.readline()
-        # line=line.strip('\r\n')
+        line=line.strip('\r\n')
         if 'wdswdswds' in line:
             break
         result.append(line)
@@ -27,7 +27,7 @@ def read_ontology(filepath):
 def write_ontology(filepath,lines):
     file_object = open(filepath,'w')
     for line in lines:
-        file_object.write(line)
+        file_object.write(line+'\n')
     file_object.close()
 
 def write_csv(filepath,llist):
@@ -35,20 +35,20 @@ def write_csv(filepath,llist):
     for line in llist:
         writer.writerow(line)
 
-def add_alias(ontology,alias_dict):
+def add_department(ontology,department_dict):
     new_ontology = []
     flag = False
 
-    alias_line = '        <oboInOwl:hasExactSynonym rdf:datatype="&xsd;string">%s</oboInOwl:hasExactSynonym>iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii\n'
+    department_line = '		<oboInOwl:part_of_body rdf:datatype="&xsd;string">%s</oboInOwl:part_of_body>'
 
     disease_aliases = None
-    disease_pattern = re.compile(r'(?<=<rdfs:label rdf:datatype="&xsd;string">).+?(?=</rdfs:label>)')
+    doid_pattern = re.compile(r'(?<=<oboInOwl:id rdf:datatype="&xsd;string">DOID:).+?(?=</oboInOwl:id>)')
     end_pattern = re.compile(r'</owl:Class>')
 
 
     for line in ontology:
 
-        line_disease = disease_pattern.findall(line)
+        line_disease = doid_pattern.findall(line)
         line_end = end_pattern.findall(line)
 
         if r'    // Classes' in line:
@@ -56,44 +56,46 @@ def add_alias(ontology,alias_dict):
 
         if len(line_disease) > 0:
             disease = line_disease[0]
-            if disease in alias_dict:
+            if disease in department_dict:
                 flag =  True
-                disease_aliases = alias_dict[disease]
+                disease_departments = department_dict[disease]
+            else:
+                flag = False
 
         elif line_end and flag:
-            for alias in disease_aliases:
-                new_ontology.append(alias_line % alias)
+            for disease_department in disease_departments:
+                new_ontology.append(department_line % disease_department)
 
         new_ontology.append(line)
 
     return new_ontology
 
-def build_alias_dict(lines):
+def build_department_dict(lines):
     dict = {}
 
     for line in lines:
-        aliases = line[1:]
-        name = line[0].decode()
-        dict[name] = aliases
+        department = line[1:]
+        doid = line[0].decode()
+        dict[doid] = department
 
     return dict
 
 
 if __name__ == '__main__':
-    full_ontology_filepath = u'zh_gastrointestinal_delete_nouse_subclass_test.owl'
-    new_ontology_filepath = u'zh_gastrointestinal_step1.owl'
+    full_ontology_filepath = u'zh_gastrointestinal_step3.owl'
+    new_ontology_filepath = u'zh_gastrointestinal_step4.owl'
 
-    diseaseName_filepath = 'zh_diseaseName.csv'
+    department_filepath = 'part_of_body.csv'
 
 
 
     ontology = read_ontology(full_ontology_filepath)
     print 'end read'
-    alias_dict = build_alias_dict(read_csv(diseaseName_filepath))
+    department_dict = build_department_dict(read_csv(department_filepath))
 
     print 'end read'
 
-    new_ontology = add_alias(ontology,alias_dict)
+    new_ontology = add_department(ontology,department_dict)
     write_ontology(new_ontology_filepath,new_ontology)
 
 
